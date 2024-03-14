857538b5f59209b8ebb7a2b2b27caf09ce4f9b3b
Fabien Gadet <fabiengadet@gmail.com>
2024-03-14 17:56:50 +0800

    today

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ mod vault;
 use vault::*;
 
 fn main() {
-    let v = Vault {
-        name: String::from("test"),
-        path: String::from("./"),
-        encoding: String::from(""),
-    };
+    let v = Vault::new("test");
+
     v.create_vault().unwrap();
-    v.new_password(&"gmail".to_string(), &"test".to_string());
-    v.new_password(&"yo".to_string(), &"test".to_string());
-    v.new_password(&"w".to_string(), &"test".to_string());
-    v.new_password(&"q".to_string(), &"test".to_string());
+    v.new_password("gmail", "test").unwrap();
+    // v.new_password("s", "test").unwrap();
+    // v.new_password("21", "test").unwrap();
+    // v.new_password("0123", "test").unwrap();
+    // v.new_password("gmail", "test").unwrap();
+    // v.new_password(&"yo".to_string(), &"test".to_string());
+    // v.new_password(&"w".to_string(), &"test".to_string());
+    // v.new_password(&"q".to_string(), &"test".to_string());
 }
--- src/vault.rs
+++ src/vault.rs
@@ -1,30 +1,52 @@
-use std::io::Write;
+use std::{collections::HashMap, fs, io::Write};
 
 const PATH_ROOT: &str = "./my_vault";
+const PASS_FILE_NAME: &str = "secret";
 
 pub struct Vault {
-    pub name: String,
-    pub path: String,
-    pub encoding: String,
+    name: String,
+    path: String,
+    encoding: String,
+    passwords: HashMap<String, String>,
 }
 
 impl Vault {
+    pub fn new(name: &str) -> Vault {
+        Vault {
+            name: name.to_string(),
+            path: "./".to_string(),
+            encoding: "".to_string(),
+            passwords: HashMap::new(),
+        }
+    }
+
     pub fn create_vault(&self) -> std::io::Result<()> {
         let full_path = PATH_ROOT.to_string() + "/" + &self.name;
         self.create_dir(&full_path)
     }
 
-    pub fn create_dir(&self, path: &String) -> std::io::Result<()> {
+    pub fn create_dir(&self, path: &str) -> std::io::Result<()> {
         std::fs::create_dir_all(path)
     }
 
-    fn get_full_path(&self, file_name: &String) -> String {
-        PATH_ROOT.to_string() + "/" + &self.name + "/" + &file_name
+    fn get_main_directory_path(&self) -> String {
+        PATH_ROOT.to_string() + "/" + &self.name
     }
 
-    pub fn new_password(&self, key: &String, value: &String) -> std::io::Result<()> {
-        let mut fd = std::fs::File::create(self.get_full_path(key))?;
-        fd.write(value.as_bytes())?;
+    fn get_main_file_path(&self) -> String {
+        PATH_ROOT.to_string() + "/" + &self.name + "/" + PASS_FILE_NAME
+    }
+    // fn parse(&self) -> std::io::Result<()>{
+    //     let buf = fs::read_to_string(self.get_main_file_path())?;
+    //     for (line: String) in buf{
+    //         line.split
+    //         self.passwords.entry(key).unwrap_or(value);
+    //     }
+    //
+    // }
+    pub fn new_password(&self, key: &str, value: &str) -> std::io::Result<()> {
+        let mut fd = std::fs::File::create(self.get_main_file_path())?;
+        fd.write((key.to_string() + "=" + value).as_bytes())?;
         Ok(())
     }
 }
